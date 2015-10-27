f950eea660eb9910fe30fe7c5923ce22d4dfa2f6
lphilip <lphilip@usc.edu>
2015-10-27 03:58:45 -0700

    Create header.h

---
+++ header.h
@@ -0,0 +1,14 @@
+/*RoughDraft.v1.0 */
+
+
+#include "vxWorks.h"
+#include "msgQLib.h"
+#include "time.h"
+#include "iostream"
+#include "fstream"
+
+#define MSGLEN 1024
+#define MAX_MSG 10
+
+MSG_Q_ID QueueID;
+using namespace std;
