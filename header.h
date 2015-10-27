7377c25df774feca1095379f5e0bd7edf960450d
lphilip <lphilip@usc.edu>
2015-10-27 03:59:54 -0700

    Update header.h

--- header.h
+++ header.h
@@ -6,9 +6,17 @@
 #include "time.h"
 #include "iostream"
 #include "fstream"
+#include "ioLib.h"
 
 #define MSGLEN 1024
 #define MAX_MSG 10
 
 MSG_Q_ID QueueID;
 using namespace std;
+
+struct YPR
+ {
+   double pitch;
+   double roll;
+   double yaw;
+ }ypr;
